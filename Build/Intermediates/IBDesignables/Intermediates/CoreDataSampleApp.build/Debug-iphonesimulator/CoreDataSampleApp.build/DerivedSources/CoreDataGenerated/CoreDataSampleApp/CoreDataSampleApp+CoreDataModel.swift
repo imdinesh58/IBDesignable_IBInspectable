13fd875ef9418c0ce86df1cfbc36782754e43b57
NirmalKumar <imdinesh.58@gmail.com>
2017-11-20 16:50:39 +0530

    IBDesignable_IBInspectable
    IBDesignable_IBInspectable

---
+++ Build/Intermediates/IBDesignables/Intermediates/CoreDataSampleApp.build/Debug-iphonesimulator/CoreDataSampleApp.build/DerivedSources/CoreDataGenerated/CoreDataSampleApp/CoreDataSampleApp+CoreDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataSampleApp+CoreDataModel.swift
+//  
+//
+//  Created by Apple-1 on 11/20/17.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
